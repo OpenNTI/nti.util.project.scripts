03a5bdc97a673223c96689f005bce71b46db0eb6
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-06-10 22:35:45 -0500

    wip: init script notes

--- packages/nti-lib-scripts/tasks/init.js
+++ packages/nti-lib-scripts/tasks/init.js
@@ -1,1 +1,14 @@
 'use strict';
+//Update package.json:
+// 1) remove our deps from devdeps.
+// 2) add ourselves as dev deps
+// 3) remove jest, jest-junit keys.
+// 4) update scripts:
+// 	a) unset 'preversion', 'postversion', 'bump', 'prebump', 'postbump'
+// 	b) set "test": "nti-lib-scripts test"
+// 	c) set "start": "nti-lib-scripts test --watch"
+// 	d) set "prepublish": "nti-lib-scripts build"
+
+//Replace .babelrc, .editorconfig, .eslintignore, .eslintrc, .npmignore
+//Remove rollup.config.js, karma.config.js, Makefile
+//Remove '/test' dir
