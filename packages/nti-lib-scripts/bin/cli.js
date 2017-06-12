a5a407f040a87e9a382151bc4071d22dd6f555c6
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-06-12 14:01:54 -0500

    lib: add clean command

--- packages/nti-lib-scripts/bin/cli.js
+++ packages/nti-lib-scripts/bin/cli.js
@@ -9,7 +9,7 @@ const script = process.argv[2];
 const args = process.argv.slice(3);
 
 
-if (!['build', 'check', 'init', 'release', 'test'].includes(script)) {
+if (!['build', 'clean', 'check', 'init', 'release', 'test'].includes(script)) {
 	console.log('Unknown task "' + script + '".');
 	process.exit(1);
 }
