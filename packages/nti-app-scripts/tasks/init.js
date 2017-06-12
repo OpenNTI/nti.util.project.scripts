7d5345e64875267a6c0224e2586b99857793bd35
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-06-12 12:53:13 -0500

    allow super-scripts to extend base-scripts

--- packages/nti-app-scripts/tasks/init.js
+++ packages/nti-app-scripts/tasks/init.js
@@ -1,1 +1,4 @@
 'use strict';
+global.NTI_INIT_TO_COPY = [];
+global.NTI_INIT_TO_REMOVE = [];
+require('nti-lib-scripts/tasks/init');
--- packages/nti-lib-scripts/tasks/init.js
+++ packages/nti-lib-scripts/tasks/init.js
@@ -7,16 +7,20 @@ const call = require('./utils/call-cmd');
 const readPackageJson = require('./utils/read-package-json');
 const writePackageJson = require('./utils/write-package-json');
 
+const currentScriptsPaths = require(path.resolve(path.dirname(process.argv[1]), '../config/paths'));
+
 const {json: pkg, indent} = readPackageJson();
-const {json: ownPkg} = readPackageJson(paths.ownPackageJson);
-const dropDeps = ['babel-register', ...Object.keys(ownPkg.dependencies)];
+const {json: libPkg} = readPackageJson(paths.ownPackageJson);
+const {json: ownPkg} = readPackageJson(currentScriptsPaths.ownPackageJson);
+const dropDeps = ['babel-register', ...Object.keys(Object.assign({}, libPkg.dependencies, ownPkg.dependencies))];
+const scriptPackageName = ownPkg.name;
 
 const write = x => console.log(chalk.cyan('\n' + x));
 
 write(`Updating: ${chalk.magenta.underline('pacakge.json')}`);
 //Update package.json:
 // 1) add ourselves as dev deps
-pkg.devDependencies[ownPkg.name] = '^' + ownPkg.version;
+pkg.devDependencies[scriptPackageName] = '^' + ownPkg.version;
 // 2) remove our deps from devdeps.
 for (let dep of dropDeps) {
 	delete pkg.devDependencies[dep];
@@ -33,35 +37,57 @@ delete pkg.scripts['postbump'];
 delete pkg.scripts['version'];
 delete pkg.scripts['preversion'];
 delete pkg.scripts['postversion'];
-// 	b) set "test": "nti-lib-scripts test"
-pkg.scripts['test'] = 'nti-lib-scripts test';
-// 	c) set "start": "nti-lib-scripts test --watch"
-pkg.scripts['start'] = 'nti-lib-scripts test --watch';
-// 	d) set "prepublish": "nti-lib-scripts build"
-pkg.scripts['prepublish'] = 'nti-lib-scripts build';
+delete pkg.scripts['prepublish'];
+// 	b) set "test": "${scriptPackageName} test"
+pkg.scripts['test'] = `${scriptPackageName} test`;
+// 	c) set "start": "${scriptPackageName} test --watch"
+pkg.scripts['start'] = `${scriptPackageName} test --watch`;
+// 	d) set "prepublish": "${scriptPackageName} build"
+pkg.scripts['prepack'] = `${scriptPackageName} build`;
+pkg.scripts['release'] = `${scriptPackageName} release`;
 
 // save:
 writePackageJson(pkg, {spaces: indent});
 
 //Replace .babelrc, .editorconfig, .eslintignore, .eslintrc, .npmignore
-const ToCopy = ['.babelrc', '.editorconfig', '.eslintignore', '.eslintrc', '.npmignore'];
+const ToCopy = [
+	'.babelrc',
+	'.editorconfig',
+	'.eslintignore',
+	'.eslintrc',
+	'.npmignore',
+	...(global.NTI_INIT_TO_COPY || [])
+];
 write(`Updating/Adding: ${chalk.magenta(ToCopy.join(', '))}`);
 for (let file of ToCopy) {
+	const lib = path.resolve(paths.ownPath, 'config', 'init-files', file);
+	const cur = path.resolve(currentScriptsPaths.ownPath, 'config', 'init-files', file);
+
 	fs.copySync(
-		path.resolve(paths.ownPath, 'config', 'init-files', file),
+		fs.existsSync(lib) ? lib : cur,
 		path.resolve(paths.path, file)
 	);
 }
 
 //Remove rollup.config.js, karma.config.js, Makefile
 //Remove './test' dir
-const ToRemove = ['rollup.config.js', 'karma.config.js', 'Makefile', 'test', 'package-lock.json', 'node_modules'];
+const ToRemove = [
+	'rollup.config.js',
+	'karma.config.js',
+	'Makefile',
+	'test',
+	'release.sh',
+	'snapshot.sh',
+	...(global.NTI_INIT_TO_REMOVE || []),
+	'package-lock.json',
+	'node_modules',
+];
 write(`Removing file/dirs that are now managed: ${chalk.magenta(ToRemove.join(', '))}`);
 for (let file of ToRemove) {
 	fs.removeSync(path.resolve(paths.path, file));
 }
 
 write(`Regenerate: ${chalk.magenta('package-lock.json', 'node_modules')}...`);
-call('npm', ['install']);
+call('npm', ['install', '--no-progress'], {stdio: null});
 
 write('Done.');
