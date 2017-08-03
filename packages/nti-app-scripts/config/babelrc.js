9322ab28fac155488d32bec359ca7818191ffcff
Jonathan Grimes <jonathan.grimes@nextthought.com>
2017-08-03 17:32:18 -0500

    add some jsx helpers

--- packages/nti-app-scripts/config/babelrc.js
+++ packages/nti-app-scripts/config/babelrc.js
@@ -41,3 +41,18 @@ else {
 		]
 	});
 }
+
+if (env === 'development' || env === 'test') {
+	// The following two plugins are currently necessary to make React warnings
+	// include more valuable information. They are included here because they are
+	// currently not enabled in babel-preset-react. See the below threads for more info:
+	// https://github.com/babel/babel/issues/4702
+	// https://github.com/babel/babel/pull/3540#issuecomment-228673661
+	// https://github.com/facebookincubator/create-react-app/issues/989
+	module.exports.plugins.push(
+		// Adds component stack to warning messages
+		require.resolve('babel-plugin-transform-react-jsx-source'),
+		// Adds __self attribute to JSX which React will use for some warnings
+		require.resolve('babel-plugin-transform-react-jsx-self')
+	);
+}
