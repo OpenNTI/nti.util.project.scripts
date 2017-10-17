037ea91ffc1bbdb845e945b6709b2846d6ce97a8
Jonathan Grimes <jsg2021@gmail.com>
2017-10-17 18:00:21 -0500

    if the repo has upstream changes, prompt what to do.

--- packages/nti-lib-scripts/tasks/release.js
+++ packages/nti-lib-scripts/tasks/release.js
@@ -3,6 +3,7 @@ const chalk = require('chalk');
 const path = require('path');
 const gitState = require('git-state');
 const semver = require('semver');
+const inquirer = require('inquirer');
 const paths = require('../config/paths');
 const readPackageJson = require('./utils/read-package-json');
 const call = require('./utils/call-cmd');
@@ -24,8 +25,12 @@ if (!gitState.isGitSync(paths.path)) {
 	process.exit(1);
 }
 
+//Always work with current data...
+call('git', ['fetch']);
 
-const {branch, dirty} = gitState.checkSync(paths.path);
+
+const {branch, dirty, behind, remoteBranch} = gitState.checkSync(paths.path);
+const hasRemote = Boolean(remoteBranch);
 
 const inc = branch === 'master' ? (major ? 'major' : 'minor') : 'patch';
 
@@ -46,47 +51,93 @@ if (branch === 'master' && !/-alpha$/.test(pkg.version)) {
 	process.exit(1);
 }
 
-write(chalk.cyan('Working on branch: ' + chalk.underline.magenta(branch)));
+const questions = [
+	{
+		type: 'list',
+		name: 'behind',
+		prefix: '',
+		suffix: '',
+		message: '\n\n' + chalk.red(chalk.underline(paths.path)) + ' has upstream changes...',
+		defualt: 'exit',
+		when: hasRemote && behind > 0,
+		choices: [
+			{
+				name: 'Pull changes, and continue',
+				value: 'pull',
+				short: 'pulling...'
+			},
+			{
+				name: 'Exit. (You will  need to deal with them manaully)',
+				value: 'exit',
+				short: 'exiting...'
+			},
+		]
+	},
+];
 
-for (let task of tasks) {
-	write(chalk.cyan('\nRunning: Task: ' + chalk.underline.magenta(task)));
-	call(command, [task]);
+inquirer.prompt(questions)
+	.then(answers => (onBehind(answers.behind), answers))
+	.then(performRelease)
+	.catch(e => {
+		write('\n\nOuch... \n',e.stack || e);
+	});
+
+
+function onBehind (e) {
+	if (!e) {return;}
+	if (e === 'exit') {
+		write('\n\n');
+		process.exit();
+	}
+
+	call('git', ['pull', '-r']);
 }
 
-const version = semver.inc(pkg.version, inc);
-const newTag = `v${version}`;
-const nextVersion = semver.inc(version, 'minor') + '-alpha';
 
-// npm --no-git-tag-version version $VERSION > /dev/null
-write(chalk.cyan(`\nSetting release version: ${chalk.underline.magenta(version)}...`));
-call('npm', ['--no-git-tag-version', 'version', version], {stdio: null});
+function performRelease () {
+	write(chalk.cyan('Working on branch: ' + chalk.underline.magenta(branch)));
 
-write(chalk.cyan(`\nCommiting release version ${chalk.underline.magenta(version)}, tagging...`));
-// git add package.json package-lock.json
-call('git', ['add', 'package.json', 'package-lock.json']);
-// git commit -m "$VERSION" > /dev/null
-call('git', ['commit', '-m', version]);
-// git tag "v$VERSION" -m "Cut on $DATE"
-call('git', ['tag', newTag, '-m', `Cut on ${DATE}`]);
+	for (let task of tasks) {
+		write(chalk.cyan('\nRunning: Task: ' + chalk.underline.magenta(task)));
+		call(command, [task]);
+	}
 
+	const version = semver.inc(pkg.version, inc);
+	const newTag = `v${version}`;
+	const nextVersion = semver.inc(version, 'minor') + '-alpha';
 
-if (branch === 'master') {
-	write(chalk.cyan(`\nSetting up next release version: ${chalk.underline.magenta(nextVersion)}...`));
 	// npm --no-git-tag-version version $VERSION > /dev/null
-	call('npm', ['--no-git-tag-version', 'version', nextVersion], {stdio: null});
+	write(chalk.cyan(`\nSetting release version: ${chalk.underline.magenta(version)}...`));
+	call('npm', ['--no-git-tag-version', 'version', version], {stdio: null});
 
+	write(chalk.cyan(`\nCommiting release version ${chalk.underline.magenta(version)}, tagging...`));
 	// git add package.json package-lock.json
 	call('git', ['add', 'package.json', 'package-lock.json']);
 	// git commit -m "$VERSION" > /dev/null
-	call('git', ['commit', '-m', nextVersion]);
+	call('git', ['commit', '-m', version]);
+	// git tag "v$VERSION" -m "Cut on $DATE"
+	call('git', ['tag', newTag, '-m', `Cut on ${DATE}`]);
+
+
+	if (branch === 'master') {
+		write(chalk.cyan(`\nSetting up next release version: ${chalk.underline.magenta(nextVersion)}...`));
+		// npm --no-git-tag-version version $VERSION > /dev/null
+		call('npm', ['--no-git-tag-version', 'version', nextVersion], {stdio: null});
+
+		// git add package.json package-lock.json
+		call('git', ['add', 'package.json', 'package-lock.json']);
+		// git commit -m "$VERSION" > /dev/null
+		call('git', ['commit', '-m', nextVersion]);
+	}
+
+	write(chalk.cyan(`\n${chalk.underline('Pushing')} changes to remote...`));
+	call('git', ['push', 'origin', 'tag', newTag]);
+
+	if (hasRemote) {
+		call('git', ['push']);
+	}
+
+
+	write('\n');
+	write(chalk.magenta('Done.\n\n'));
 }
-
-write(chalk.cyan(`\n${chalk.underline('Pushing')} changes to remote...`));
-call('git', ['push', 'origin', 'tag', newTag]);
-if (branch === 'master') {
-	call('git', ['push']);
-}
-
-
-write('\n');
-write(chalk.magenta('Done.\n\n'));
