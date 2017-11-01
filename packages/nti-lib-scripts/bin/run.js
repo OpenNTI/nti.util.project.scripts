88f39c52827b3aa6f0503de0489a23c373216af0
Jonathan Grimes <jsg2021@gmail.com>
2017-11-01 18:12:27 -0500

    update the command runner to allow some scripts to run, but still warn

--- packages/nti-lib-scripts/bin/run.js
+++ packages/nti-lib-scripts/bin/run.js
@@ -5,19 +5,48 @@ const path = require('path');
 
 const chalk = require('chalk');
 
+const readPackageJson = require('../tasks/utils/read-package-json');
+
 const [, file, ...argv] = process.argv;
 const script = path.basename(file);
 const args = argv.length ? ['--', ...argv] : [];
 
 const write = (x) => console.log(x);
 
-const pkg = require(`${process.cwd()}/package.json`);
+const {json: pkg} = readPackageJson();
 const name = pkg.name;
 const scripts = pkg.scripts || {};
 
 if (!scripts[script]) {
-	write(chalk.red(`\n\nThis project (${chalk.underline(chalk.bold(name))}) does not define the "${chalk.underline(chalk.bold(script))}" script.\n\n`));
-	process.exit(1);
+	write(chalk.red(`\n\nThis project (${chalk.underline(chalk.bold(name))}) does not define the "${chalk.underline(chalk.bold(script))}" script.`));
+	if (!pkg.scripts || !scripts.test || !scriptExists(script)) {
+		write('\n\n');
+		process.exit(1);
+	}
+
+	scripts[script] = scripts.test.replace(/test$/, script);
+	write(chalk.red(`⚠️  Assuming: "${chalk.bold(scripts[script])}"... add to package to remove this warning.\n\n`));
+
+	const [cmd, ...sargs] = scripts[script].split(/\s+/);
+
+	args.unshift();//remove '--' since we aren't using NPM to run the script.
+
+	spawn(cmd, [...sargs, ...args], { stdio: 'inherit' });
+	return;
 }
 
 spawn('npm', ['run', script, ...args], { stdio: 'inherit' });
+
+
+
+function scriptExists (s) {
+	try {
+		if (!require.resolve(scripts.test.split(/\s+/)[0] + '/tasks/' + s)) {
+			throw new Error('nope');
+		}
+	} catch (e) {
+		return false;
+	}
+
+	return true;
+}
