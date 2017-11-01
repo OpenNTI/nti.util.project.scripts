22dc2801d0ca01e40eeb8357d5618fe19c65e4ac
Jonathan Grimes <jsg2021@gmail.com>
2017-11-01 17:29:44 -0500

    let there be 'fix'

--- packages/nti-lib-scripts/tasks/fix.js
+++ packages/nti-lib-scripts/tasks/fix.js
@@ -3,33 +3,59 @@ const chalk = require('chalk');
 const inquirer = require('inquirer');
 const {spawnSync} = require('child_process');
 const semver = require('semver');
+const gitState = require('git-state');
 
+const cwd = process.cwd();
 
 const getMaintBranchName = (v) => (v = semver.parse(v), v && (v.patch = '', `maint-${v.format().replace(/\.$/, '')}`));
 const normalizeVersion = x => (x = semver.parse(x), x && x.format());
 
-const write = (x) => console.log(x);
+const write = (...x) => console.log(...x);
+const toList = (x) => x && x.stdout && x.stdout.toString('utf8').split(/[\r\n]+/).filter(Boolean);
 const call = (cmd, ...args) => spawnSync(cmd, args, {
-	cwd: '/Users/jgrimes/Workspace/nti-web-app',
+	cwd,
 	env: Object.assign({}, process.env),
 	stdio: [null, 'pipe', null],
 	maxBuffer: 1024 * 1024 //1MB max
 });
 
 
+if (!gitState.isGitSync(cwd)) {
+	write('\n\n' + chalk.red(chalk.underline(cwd) + ' is not a git repository.') + '\n\n');
+	process.exit(1);
+}
+
+//Always work with current data...
+call('git', 'fetch');
+
+
+const {dirty} = gitState.checkSync(cwd);
+
+if (dirty) {
+	write('\n\n' + chalk.red(chalk.underline(cwd) + ' has uncommited changes.') + '\n\n');
+	process.exit(1);
+}
+
+
+
 const version = normalizeVersion(process.argv[2]);
 // write(`Arg: ${version}`);
 
-const {stdout: tags} = call('git', 'tag');
+const tags = toList(call('git', 'tag'));
+const remotes = toList(call('git', 'remote'));
+const branches = toList(call('git', 'branch', '-a')).map(x => x.replace(/^[*\s]+/, ''));
+
+if (!remotes.includes('origin')) {
+	write('This git repository does not have a remote named "origin"');
+	process.exit(1);
+}
 
 if (!tags) {
 	process.exit(1);
 }
 
-
 const list = Object.values(
-	tags.toString('utf8')
-		.split(/[\r\n]+/)
+	tags
 		.filter(semver.valid)
 		.sort(semver.compare)
 		.map(x => ({
@@ -43,7 +69,7 @@ const list = Object.values(
 	.filter(x => !version || x.maint === getMaintBranchName(version))
 	.slice(0, 5);
 
-const questions = [
+inquirer.prompt([
 	{
 		type: 'list',
 		name: 'fix',
@@ -54,26 +80,59 @@ const questions = [
 		when: list.length > 1,
 		choices: list.slice(0, 10).map(x => (
 			{
-				name: `Checkout/create ${chalk.red(x.maint)}`,
+				name: `Checkout/create ${chalk.red(x.maint)} branch`,
+				short: x.maint,
 				value: x,
 			}
 		))
 	},
-];
-
-
-
-inquirer.prompt(questions)
+])
 	.then(handleResponse)
 	.catch(e => {
 		write('\n\nOuch... \n',e.stack || e);
 	});
 
 
-function handleResponse ({fix}) {
+function handleResponse ({fix = list[0]}) {
+	// const call = write;
+	const {tag, maint} = fix;
+	const maintRemote = `remotes/origin/${maint}`;
+	const remoteBranch = branches.find(x => x === maintRemote);
+	const localBranch = branches.find(x => x === maint);
 
-	write(fix);
+
+	// write(`remote branch: ${remoteBranch}`);
+	// write(`local branch: ${localBranch}`);
+
+
+	if (localBranch) {
+		write(chalk.cyan('Exiting local branch detected, checking out...'));
+		call('git', 'checkout', localBranch);
+
+		if (remoteBranch) {
+			write(chalk.cyan('Pulling from remote...'));
+			call('git', 'pull', '-r');
+		}
+
+	} else {
+		write(chalk.cyan(`Creating a new local branch starting at: ${chalk.underline.magenta(tag)}`));
+		call('git', 'checkout', tag);
+		call('git', 'checkout', '-b', maint);
+
+		if (!remoteBranch) {
+			write(chalk.cyan('No remote branch detected, pushing new branch to remote...'));
+			call('git', 'push', '-u', 'origin');
+		} else {
+			write(chalk.cyan('Exiting remote branch detected, pulling from remote...'));
+			call('git', 'branch', maint, '--set-upstream-to', remoteBranch);
+			call('git', 'pull', '-r');
+		}
+	}
+
+	write('\n');
+	write(chalk.cyan('😎 You should now be on branch: ' + chalk.underline.magenta(maint)));
+	write(chalk.cyan('🎉 Happy bug fixing! 🐛'));
+	write('\n\n');
+	write(chalk.cyan(`When you are ready to release a patch simply run: ${chalk.bold('release')} from this branch. (yes, its a command 😀)`));
+	write('\n\n');
 }
-
-//call('git', ['checkout', version]);
-//call('git', ['checkout', '-b', `maint-${baseVersion}`]);
