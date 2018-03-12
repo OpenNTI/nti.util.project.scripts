e84ed416179631d36108a427cefe42840de3a94a
Jonathan Grimes <jsg2021@gmail.com>
2018-03-12 16:55:12 -0500

    cleanup old JSX lint rules.

--- packages/eslint-config-nti-codestyle-jsx/index.js
+++ packages/eslint-config-nti-codestyle-jsx/index.js
@@ -38,23 +38,21 @@ module.exports = {
 		//See: http://eslint.org/docs/rules/jsx-quotes
 		'jsx-quotes': ['warn', 'prefer-double'],
 
-		//Standarize on unary/valueless attributes for constant boolean props.
-		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-boolean-value.md
-		'react/jsx-boolean-value': 'warn',
-
-		//Using bind() or arrow functions on props always trigger a prop-change.
-		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-bind.md
-		'react/jsx-no-bind': 'error',
 
 		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-pascal-case.md
 		'react/jsx-pascal-case': ['warn', { 'allowAllCaps': true }],
 
-		//All child-less elements should self close. ex:  <div></div> should be <div/>
-		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
-		'react/self-closing-comp': 'error',
 
 		//JSX should always be wrapped in parentheses... especically when multiline.
 		//See: https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/wrap-multilines.md
-		'react/jsx-wrap-multilines': ['error', {declaration: true, assignment: true, return: true}]
+		'react/jsx-wrap-multilines': ['warn', {
+			'declaration': 'parens-new-line',
+			'assignment': 'parens-new-line',
+			'return': 'parens-new-line',
+			'arrow': 'parens-new-line',
+			'condition': 'parens-new-line',
+			'logical': 'parens-new-line',
+			'prop': 'parens-new-line'
+		}]
 	}
 };
