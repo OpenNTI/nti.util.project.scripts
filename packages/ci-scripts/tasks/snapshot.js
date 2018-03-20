57d6bdc1a49ef0a01bb0900bd653bc5b07c4f3be
Jonathan Grimes <jsg2021@gmail.com>
2018-03-20 14:00:08 -0500

    simplify snapshot tag

--- packages/ci-scripts/tasks/snapshot.js
+++ packages/ci-scripts/tasks/snapshot.js
@@ -10,6 +10,5 @@ call(`npm --no-git-tag-version version ${version}.${stamp}`, silent);
 //publish the snapshot (will build)
 call('npm publish --tag alpha');
 
-call('git tag --delete snapshot', nofail);
-call('git tag snapshot', silent);
+call('git tag snapshot -f', nofail);
 call('git push origin tag snapshot -f', silent);
