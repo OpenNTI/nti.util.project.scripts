6e14e56523c8e7c186497603c0012ca1e3e5b9c6
Jonathan Grimes <jsg2021@gmail.com>
2018-03-20 12:46:05 -0500

    silence git tags

--- packages/ci-scripts/tasks/snapshot.js
+++ packages/ci-scripts/tasks/snapshot.js
@@ -3,12 +3,13 @@
 const {prepare, call, nofail} = require('./util/prepare');
 
 const {version, stamp} = prepare('snapshot');
+const silent = {fd:'ignore'};
 
 // Update the package(-lock).json to a snapshot version
-call(`npm --no-git-tag-version version ${version}.${stamp}`, {fd: 'ignore'});
+call(`npm --no-git-tag-version version ${version}.${stamp}`, silent);
 //publish the snapshot (will build)
 call('npm publish --tag alpha');
 
 call('git tag --delete snapshot', nofail);
-call('git tag snapshot');
-call('git push origin tag snapshot -f');
+call('git tag snapshot', silent);
+call('git push origin tag snapshot -f', silent);
