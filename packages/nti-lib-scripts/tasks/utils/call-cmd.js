11576993e6042e0781a3bfc9beedd803091b2b68
Jonathan Grimes <jsg2021@gmail.com>
2018-03-26 16:31:49 -0500

    pass env... :/

--- packages/nti-lib-scripts/tasks/utils/call-cmd.js
+++ packages/nti-lib-scripts/tasks/utils/call-cmd.js
@@ -4,7 +4,9 @@ const {spawnSync} = require('child_process');
 const STDIO = { stdio: 'inherit' };
 
 module.exports = function call (cmd, args, opts = STDIO, printStdError = false) {
-	const result = spawnSync(cmd, args, opts);
+	const env = opts.env || process.env;
+	const result = spawnSync(cmd, args, {env, ...opts});
+
 	if (result.status) {
 		if (result.stderr && printStdError) {
 			console.error(result.stderr.toString('utf8'));
