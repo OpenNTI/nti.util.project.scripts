66eae837fe26ca161f18712f340b5ed0a2d9b1c2
Jonathan Grimes <jsg2021@gmail.com>
2018-04-03 14:37:16 -0500

    don't miss setting up env for ci

--- packages/nti-lib-scripts/config/jest/setup-env.js
+++ packages/nti-lib-scripts/config/jest/setup-env.js
@@ -2,16 +2,21 @@
 const path = require('path');
 
 module.exports = function setupEnv () {
-	const file = process.argv[1];
-	if (/tasks\/test\.js/.test(file)) {
-		return {config: path.resolve(path.dirname(process.argv[1]), '../config/')};
-	}
-
 	try {
-		const cwd = process.cwd();
-		const {scripts} = require(path.resolve(cwd, 'package.json'));
-		const [script] = scripts.test.split(' ');
-		const config = path.join(cwd, 'node_modules', script, 'config');
+		let config;
+
+		const [,file] = process.argv;
+
+		if (/tasks\/test\.js/.test(file)) {
+			config = path.resolve(path.dirname(file), '../config/');
+		}
+		else {
+			const cwd = process.cwd();
+			const {scripts} = require(path.resolve(cwd, 'package.json'));
+			const [script] = scripts.test.split(' ');
+
+			config = path.join(cwd, 'node_modules', script, 'config');
+		}
 
 		try {
 			require(path.join(config, 'jest'));
