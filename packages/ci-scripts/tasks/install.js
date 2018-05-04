24579de8416be1f477b12c33d8a32560bf3393c1
Jonathan Grimes <jsg2021@gmail.com>
2018-05-04 16:30:55 -0500

    (ci: npm install) print failure to job log

--- packages/ci-scripts/tasks/install.js
+++ packages/ci-scripts/tasks/install.js
@@ -3,16 +3,26 @@ const fs = require('fs-extra');
 const path = require('path');
 const { call, printLine, print } = require('./util');
 
+const SUCCESS = 0;
 const cwd = process.cwd();
-
-print('Installing dependencies ... ');
-call('npm install --parseable', {
-	fd: fs.openSync(path.join(cwd, '.node_modules.log'), 'w+'),
+const log = path.join(cwd, '.node_modules.log');
+const options = {
+	fd: fs.openSync(log, 'w+'),
+	forgive: true,
 	env: {
 		// NPM will not install devDependencies if NODE_ENV is set to production.
 		// We use devDependencies to declare our build tool chain. We require devDependencies to build.
 		// So override the env here.
 		NODE_ENV: 'development'
 	}
-});
-printLine('done.');
+};
+
+print('Installing dependencies ... ');
+const result = call('npm install --parseable', options);
+if (result === SUCCESS) {
+	printLine('done.');
+} else {
+	printLine('failed!');
+	printLine(fs.readFileSync(log));
+	process.exit(result);
+}
