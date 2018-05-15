ac2f4e06984b96d382c03b168dba8e28517108d0
Jonathan Grimes <jsg2021@gmail.com>
2018-05-15 13:20:50 -0500

    Revert "try an alternate webpack plugin for Closure Compiler" and keep options.
    This reverts commit ec6caa8dd289b379dbe7accfe7737260406af8ae.

--- packages/app-scripts/config/webpack.config.js
+++ packages/app-scripts/config/webpack.config.js
@@ -15,7 +15,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
 const PreloadWebpackPlugin = require('preload-webpack-plugin');
-const ClosureCompilerPlugin = require('closure-webpack-plugin');
+const ClosureCompilerPlugin = require('webpack-closure-compiler');
 //
 const gitRevision = JSON.stringify(require('@nti/util-git-rev'));
 const eslintFormatter = require('react-dev-utils/eslintFormatter');
@@ -255,16 +255,19 @@ exports = module.exports = {
 	optimization: {
 		minimize: PROD,
 		minimizer: [
-			new ClosureCompilerPlugin({mode: 'STANDARD'}, {
-				'compilation_level': 'SIMPLE',
-				// 'language_in': 'ECMASCRIPT5_STRICT',
-				'language_out': 'ECMASCRIPT5_STRICT',
-				'warning_level': 'QUIET',
-				// 'assume_function_wrapper': false,
-				'apply_input_source_maps': false,
-				'use_types_for_optimization': false,
-				'process_common_js_modules': false,
-				'rewrite_polyfills': false
+			new ClosureCompilerPlugin({
+				compiler: {
+					'compilation_level': 'SIMPLE',
+					// 'language_in': 'ECMASCRIPT5_STRICT',
+					'language_out': 'ECMASCRIPT5_STRICT',
+					'warning_level': 'QUIET',
+					// 'assume_function_wrapper': false,
+					'apply_input_source_maps': false,
+					'use_types_for_optimization': false,
+					'process_common_js_modules': false,
+					'rewrite_polyfills': false
+				},
+				concurrency: 4
 			}),
 		],
 		occurrenceOrder: true,
