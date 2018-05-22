0a77bf0917ca067a8d5b0c74284905f9c38cbf21
Jonathan Grimes <jsg2021@gmail.com>
2018-05-22 12:51:27 -0500

    since we can't access the workspace easily, print install steps to the log

--- packages/ci-scripts/tasks/install.js
+++ packages/ci-scripts/tasks/install.js
@@ -1,13 +1,9 @@
 'use strict';
-const fs = require('fs-extra');
-const path = require('path');
-const { call, printLine, print } = require('./util');
+const { call, printLine } = require('./util');
 
 const SUCCESS = 0;
-const cwd = process.cwd();
-const log = path.join(cwd, '.node_modules.log');
+
 const options = {
-	fd: fs.openSync(log, 'w+'),
 	forgive: true,
 	env: {
 		// NPM will not install devDependencies if NODE_ENV is set to production.
@@ -17,12 +13,11 @@ const options = {
 	}
 };
 
-print('Installing dependencies ... ');
-const result = call('npm install --parseable', options);
+printLine('Installing dependencies ... ');
+const result = call('npm install --no-progress --loglevel info', options);
 if (result === SUCCESS) {
 	printLine('done.');
 } else {
 	printLine('failed!');
-	printLine(fs.readFileSync(log, {encoding: 'UTF-8', flag: 'r'}));
 	process.exit(result);
 }
