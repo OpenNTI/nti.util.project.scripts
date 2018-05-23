28f2daa1b4fa365fd0ea3fd088519774569443a1
Jonathan Grimes <jsg2021@gmail.com>
2018-05-23 14:24:26 -0500

    just mangle.

--- packages/app-scripts/config/webpack.config.js
+++ packages/app-scripts/config/webpack.config.js
@@ -266,15 +266,16 @@ exports = module.exports = {
 						// https://github.com/facebook/create-react-app/pull/4234
 						ecma: 8,
 					},
-					compress: {
-						ecma: 5,
-						warnings: false,
-						// Disabled because of an issue with Uglify breaking seemingly valid code:
-						// https://github.com/facebook/create-react-app/issues/2376
-						// Pending further investigation:
-						// https://github.com/mishoo/UglifyJS2/issues/2011
-						comparisons: false,
-					},
+					compress: false,
+					// compress: {
+					// 	ecma: 5,
+					// 	warnings: false,
+					// 	// Disabled because of an issue with Uglify breaking seemingly valid code:
+					// 	// https://github.com/facebook/create-react-app/issues/2376
+					// 	// Pending further investigation:
+					// 	// https://github.com/mishoo/UglifyJS2/issues/2011
+					// 	comparisons: false,
+					// },
 					mangle: {
 						safari10: true,
 					},
