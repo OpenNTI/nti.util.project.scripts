4e38d1f4be07edaadc357f78870554dbe4dafc28
Jonathan Grimes <jsg2021@gmail.com>
2018-05-23 10:22:11 -0500

    hide lint warings on CI

--- packages/lib-scripts/tasks/check.js
+++ packages/lib-scripts/tasks/check.js
@@ -1,4 +1,5 @@
 'use strict';
+const {isCI} = require('ci-info');
 
 const paths = require('../config/paths');
 const call = require('./utils/call-cmd');
@@ -10,4 +11,10 @@ const args = process.argv.slice(2);
 process.env.NODE_ENV = process.env.NODE_ENV || 'production';
 process.env.BABEL_ENV = process.env.BABEL_ENV || process.env.NODE_ENV;
 
-call('eslint', ['--ext', '.js,.jsx', paths.src, ...args]);
+call('eslint', [
+	...(isCI ? ['--silent'] : []),
+	'--ext',
+	'.js,.jsx',
+	paths.src,
+	...args
+]);
