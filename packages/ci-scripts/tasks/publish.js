fa5b79c2074b554eb979337e46cd91f02caeaad7
Jonathan Grimes <jsg2021@gmail.com>
2018-06-04 16:20:18 -0500

    cleanup previous snapshots if they exist.

--- packages/ci-scripts/tasks/publish.js
+++ packages/ci-scripts/tasks/publish.js
@@ -2,8 +2,8 @@
 
 const { call, getPackageNameAndVersion, nofail, printLine } = require('./util');
 
-const { isSnapshot, version, publishConfig } = getPackageNameAndVersion();
-const silent = {fd:'ignore'};
+const { isSnapshot, name, version, publishConfig } = getPackageNameAndVersion();
+const silent = {fd: 'ignore', forgive: true};
 
 if (!publishConfig || !publishConfig.registry) {
 	printLine('Refusing to publish without a publishConfig.registry set');
@@ -16,12 +16,19 @@ if (isSnapshot) {
 		return process.exit(1);
 	}
 
+	const {status, stdout: prevVersion} = call(`npm view ${name} .dist-tags.alpha`, {...silent, fd: 'pipe'});
+
 	//publish the snapshot (will build)
 	call('npm publish --tag alpha');
 
 	// move the snapshot tag to the current commit
 	call('git tag snapshot -f', nofail);
 	call('git push origin tag snapshot -f', silent);
+
+	if (status === 0 && prevVersion) {
+		printLine('Removing previous snapshot: %s@%s', name, prevVersion);
+		call(`npm unpublish ${name}@${prevVersion}`, silent);
+	}
 }
 else {
 	call('npm publish');
