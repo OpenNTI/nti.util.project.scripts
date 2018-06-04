37c3e07a7659dc2916e0692eadcd62a364a4b0c2
Ray Hatfield <ray.hatfield@nextthought.com>
2018-06-04 16:30:52 -0500

    ignore known sass at-rules

--- packages/codestyle-jsx/stylelint.js
+++ packages/codestyle-jsx/stylelint.js
@@ -1,7 +1,12 @@
+'use strict';
+
 module.exports = {
-	"extends": "stylelint-config-standard",
-	"rules": {
-		"indentation": "tab",
-		"selector-list-comma-newline-after": null
+	'extends': 'stylelint-config-standard',
+	'rules': {
+		'indentation': 'tab',
+		'selector-list-comma-newline-after': null,
+		'at-rule-no-unknown': [true, {
+			'ignoreAtRules': ['function', 'if', 'each', 'include', 'mixin']
+		}]
 	}
-}
+};
