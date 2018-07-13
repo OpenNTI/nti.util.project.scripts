887c5dbf015d66bd6132179c0d327e2c02137825
Jonathan Grimes <jsg2021@gmail.com>
2018-07-13 17:34:42 -0500

    define some aliases to global that 3rd party libs may use (and will fail this check if they are not defined)

--- packages/lib-scripts/tasks/utils/sanity-check.js
+++ packages/lib-scripts/tasks/utils/sanity-check.js
@@ -6,6 +6,10 @@ const paths = require('../../config/paths');
 module.exports = function () {
 
 	try {
+		// Some 3rd party libraries use these references expecting to run in a browser...
+		// and we will ensure they exist when we use them for SSR
+		global.window = global.self = global;
+
 		console.log(chalk.green('Sanity-Checking server-side require compatibility...'));
 
 		require(paths.packageMain);
