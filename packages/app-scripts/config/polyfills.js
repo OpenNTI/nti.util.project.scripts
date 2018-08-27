88a16f44ea158385b5737c1f40a4158ef0c70e40
Jonathan Grimes <jsg2021@gmail.com>
2018-08-27 14:53:15 -0500

    add a polyfill for Element.pt.remove() -- IE

--- packages/app-scripts/config/polyfills.js
+++ packages/app-scripts/config/polyfills.js
@@ -1,6 +1,28 @@
-/*globals BUILD_SOURCE, BUILD_PACKAGE_NAME, BUILD_PACKAGE_VERSION*/
+/*globals BUILD_SOURCE, BUILD_PACKAGE_NAME, BUILD_PACKAGE_VERSION, Element, CharacterData, DocumentType*/
 'use strict';
 require('babel-polyfill');
 global['revision'] = typeof BUILD_SOURCE !== 'undefined' && BUILD_SOURCE;
 global['BUILD_PACKAGE_NAME'] = typeof BUILD_PACKAGE_NAME !== 'undefined' && BUILD_PACKAGE_NAME;
 global['BUILD_PACKAGE_VERSION'] = global['version'] = typeof BUILD_PACKAGE_VERSION !== 'undefined' && BUILD_PACKAGE_VERSION;
+
+if (typeof document !== 'undefined') {
+	// from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+	(function (arr) {
+		arr.forEach(function (item) {
+			if (item.hasOwnProperty('remove')) {
+				return;
+			}
+
+			Object.defineProperty(item, 'remove', {
+				configurable: true,
+				enumerable: true,
+				writable: true,
+				value: function remove () {
+					if (this.parentNode !== null) {
+						this.parentNode.removeChild(this);
+					}
+				}
+			});
+		});
+	})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+}
