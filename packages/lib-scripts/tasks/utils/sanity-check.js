2a307bfaba67a84fad6fcf626cd30ce03f5a0c96
Jonathan Grimes <jsg2021@gmail.com>
2018-09-05 11:54:30 -0500

    make the sanitiy check just evaluate the new module on its own.

--- packages/lib-scripts/tasks/utils/sanity-check.js
+++ packages/lib-scripts/tasks/utils/sanity-check.js
@@ -3,15 +3,25 @@ require('regenerator-runtime/runtime');
 const chalk = require('chalk');
 const paths = require('../../config/paths');
 
+const Mock = () => new Proxy(function () {}, { get: () => Mock() });
+
 module.exports = function () {
 
 	try {
 		console.log(chalk.green('Sanity-Checking node require compatibility...'));
 
+		// Just evaluate the module on its own... so we mock out its requires,
+		// we're just checking that this module can be evaluated
+
 		try {
-			//known bad modules
-			require('module')._cache[require.resolve('hls.js')] = {};
-		} catch (e) {/**/}
+			const pkg = require(paths.packageJson);
+			const mocks = require('module')._cache;
+			for (let dep of Object.keys(pkg.dependencies)) {
+				mocks[require.resolve(dep)] = Mock();
+			}
+		} catch (e) {
+			console.warn(e.stack);
+		}
 
 		require(paths.packageMain);
 
