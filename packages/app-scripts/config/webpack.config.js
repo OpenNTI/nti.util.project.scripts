b9e4dbabdb9fb376b698e5ae58310c9ff8f62710
Jonathan Grimes <jsg2021@gmail.com>
2018-10-25 11:07:51 -0500

    try terser instead of uglify (also, re-enable simplify babel transform)

--- packages/app-scripts/config/webpack.config.js
+++ packages/app-scripts/config/webpack.config.js
@@ -14,6 +14,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackHarddiskPlugin = require('html-webpack-harddisk-plugin');
 const HtmlWebpackIncludeAssetsPlugin = require('html-webpack-include-assets-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 //
 const gitRevision = JSON.stringify(require('@nti/util-git-rev'));
 const eslintFormatter = require('react-dev-utils/eslintFormatter');
@@ -221,7 +222,7 @@ exports = module.exports = {
 										PROD && [require.resolve('babel-preset-minify'), {
 											mangle: false,
 											deadcode: false,
-											simplify: false,
+											// simplify: false,
 											evaluate: false,
 										}]
 									].filter(Boolean)
@@ -298,6 +299,46 @@ exports = module.exports = {
 
 	optimization: {
 		minimize: PROD,
+		minimizer: [PROD && new TerserPlugin({
+			terserOptions: {
+				parse: {
+					// we want terser to parse ecma 8 code. However, we don't want it
+					// to apply any minfication steps that turns valid ecma 5 code
+					// into invalid ecma 5 code. This is why the 'compress' and 'output'
+					// sections only apply transformations that are ecma 5 safe
+					// https://github.com/facebook/create-react-app/pull/4234
+					ecma: 8
+				},
+				compress: {
+					ecma: 5,
+					warnings: false,
+					// Disabled because of an issue with Uglify breaking seemingly valid code:
+					// https://github.com/facebook/create-react-app/issues/2376
+					// Pending further investigation:
+					// https://github.com/mishoo/UglifyJS2/issues/2011
+					comparisons: false,
+					// Disabled because of an issue with Terser breaking valid code:
+					// https://github.com/facebook/create-react-app/issues/5250
+					// Pending futher investigation:
+					// https://github.com/terser-js/terser/issues/120
+					inline: 2,
+
+					passes: 2
+				},
+				mangle: {
+					safari10: true
+				},
+				output: {
+					ecma: 5,
+					comments: false,
+					// Turned on because emoji and regex is not minified properly using default
+					// https://github.com/facebook/create-react-app/issues/2488
+					ascii_only: true
+				}
+			},
+			parallel: true,
+			cache: true,
+		})].filter(Boolean),
 		sideEffects: true,
 		splitChunks: {
 			chunks: 'all',
