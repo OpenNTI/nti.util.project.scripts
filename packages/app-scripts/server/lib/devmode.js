6295a2c0f6b30ea02223840a0b62c8d4035e2c25
Jonathan Grimes <jsg2021@gmail.com>
2019-05-21 14:28:56 -0500

    update ssl configuration, make it sharable

--- packages/app-scripts/server/lib/devmode.js
+++ packages/app-scripts/server/lib/devmode.js
@@ -2,14 +2,11 @@
 'use strict';
 const {worker} = require('cluster');
 const readline = require('readline');
-const chalk = require('chalk');
-const fs = require('fs');
 const url = require('url');
-const path = require('path');
+const { HTTPS, getHTTPS } = require('@nti/dev-ssl-config');
+
 
 const first = x => Array.isArray(x) ? x[0] : x;
-
-const {NTI_BUILDOUT_PATH = false} = process.env;
 
 exports.setupDeveloperMode = async function setupDeveloperMode (config) {
 	const webpack = require('webpack');
@@ -28,28 +25,13 @@ exports.setupDeveloperMode = async function setupDeveloperMode (config) {
 
 	const devPort = config['webpack-dev-server'] || await getPort();
 
-	const webpackConfig = Object.assign({}, first(webpackConfigFile));
+	const webpackConfig = { ...first(webpackConfigFile)};
 
 	webpackConfig.output.path = '/';
 	webpackConfig.output.publicPath = config.basepath;
 
-	if (!NTI_BUILDOUT_PATH) {
-		console.error(`
 
-
-		${chalk.bold(chalk.red('ERROR:'))} The environment variable ${chalk.bold('NTI_BUILDOUT_PATH')} is not defined!
-
-		SSL will not be available to webpack dev client until this is
-		defined and pointing to your buildout directory.
-
-		You should add this variable to your shell’s profile or in
-		your virtualenv workon hook.
-
-
-		`.replace(/^(\t)+/gm, '\t'));
-	}
-
-	if (devPort !== 0 && NTI_BUILDOUT_PATH) {
+	if (devPort !== 0 && HTTPS) {
 		for (let entry of Object.keys(webpackConfig.entry)) {
 			const e = webpackConfig.entry[entry];
 			e.unshift(`webpack-dev-server/client?http://${domain}:${devPort}`);
@@ -67,12 +49,9 @@ exports.setupDeveloperMode = async function setupDeveloperMode (config) {
 			'*': `http://${apiHost}:${apiPort}/`
 		},
 
-		https: NTI_BUILDOUT_PATH && {
-			cert: fs.readFileSync(path.join(NTI_BUILDOUT_PATH, 'etc/pki/localhost.crt')),
-			key: fs.readFileSync(path.join(NTI_BUILDOUT_PATH, 'etc/pki/localhost.key'))
-		},
+		https: HTTPS && getHTTPS(),
 		contentBase: paths.assetsRoot,
-		overlay: NTI_BUILDOUT_PATH && {
+		overlay: {
 			errors: true,
 			warnings: false,
 		},
--- packages/cmp-scripts/tasks/start.js
+++ packages/cmp-scripts/tasks/start.js
@@ -1,34 +1,10 @@
 'use strict';
-const chalk = require('chalk');
-const path = require('path');
 const call = require('@nti/lib-scripts/tasks/utils/call-cmd');
+const { SSL } = require('@nti/dev-ssl-config');
 const paths = require('../config/paths');
 
 const host = '0.0.0.0';
 const port = 8000;
-const {NTI_BUILDOUT_PATH = false} = process.env;
-
-const SSL = !NTI_BUILDOUT_PATH ? [] : [
-	'--https',
-	'--key', path.join(NTI_BUILDOUT_PATH, 'etc/pki/localhost.key'),
-	'--cert', path.join(NTI_BUILDOUT_PATH, 'etc/pki/localhost.crt')
-];
-
-if (!NTI_BUILDOUT_PATH) {
-	console.error(`
-
-
-	${chalk.bold(chalk.red('ERROR:'))} The environment variable ${chalk.bold('NTI_BUILDOUT_PATH')} is not defined!
-
-	SSL will not be available until this is defined and pointing to your
-	buildout directory.
-
-	You should add this variable to your shell’s profile or in
-	your virtualenv workon hook.
-
-
-	`);
-}
 
 call(require.resolve('webpack-dev-server/bin/webpack-dev-server.js'), [
 	'-d',
