c3bb3a38cb90119142f77cdb6fff2bc8aab2ecb9
Jonathan Grimes <jsg2021@gmail.com>
2019-05-28 13:47:44 -0500

    Add env var to set browserlist

--- packages/lib-scripts/config/browserlist.js
+++ packages/lib-scripts/config/browserlist.js
@@ -1,6 +1,7 @@
 'use strict';
 // Test queries and coverage here: http://browserl.ist
-module.exports = [
+const { NTI_DEV_BROWSER } = process.env;
+module.exports = NTI_DEV_BROWSER ? NTI_DEV_BROWSER.split(',') : [
 	'> 1% in US',
 	'last 2 versions',
 	'not dead',
