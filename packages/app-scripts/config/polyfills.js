4356303a0486646671b680752d0143b3c6560bc3
Jonathan Grimes <jsg2021@gmail.com>
2019-08-29 12:58:00 -0500

    lint

--- packages/app-scripts/config/polyfills.js
+++ packages/app-scripts/config/polyfills.js
@@ -11,7 +11,7 @@ if (typeof document !== 'undefined') {
 	// from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
 	(function (arr) {
 		arr.forEach(function (item) {
-			if (item.hasOwnProperty('remove')) {
+			if (Object.prototype.hasOwnProperty.call(item,'remove')) {
 				return;
 			}
 
