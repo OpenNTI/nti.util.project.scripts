f3f3dd527109483a31f84ff74309575b3d8f8770
Jonathan Grimes <jsg2021@gmail.com>
2019-11-07 09:36:03 -0600

    invalidate cache if browserlist query changes

--- packages/app-scripts/config/cache.js
+++ packages/app-scripts/config/cache.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const path = require('path');
+const bl = require('@nti/lib-scripts/config/browserlist');
 
 const paths = require('./paths');
 
@@ -8,6 +9,7 @@ const cache = (options = {}) => ({
 	loader: 'cache-loader',
 	options: {
 		cacheDirectory: path.resolve(paths.path, 'node_modules/.cache/nti-build'),
+		cacheIdentifier: `-cache- ${bl.join(',')} ${process.env.NODE_ENV}`,
 		...options
 	}
 });
