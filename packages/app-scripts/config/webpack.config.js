1cd965403aa33dbad3d0266ac2112821c7f9a97a
Jonathan Grimes <jsg2021@gmail.com>
2019-11-07 11:27:49 -0600

    chore: sync Terser config with react-scripts

--- packages/app-scripts/config/webpack.config.js
+++ packages/app-scripts/config/webpack.config.js
@@ -195,8 +195,8 @@ exports = module.exports = {
 		minimizer: [PROD && new TerserPlugin({
 			terserOptions: {
 				parse: {
-					// we want terser to parse ecma 8 code. However, we don't want it
-					// to apply any minfication steps that turns valid ecma 5 code
+					// We want terser to parse ecma 8 code. However, we don't want it
+					// to apply any minification steps that turns valid ecma 5 code
 					// into invalid ecma 5 code. This is why the 'compress' and 'output'
 					// sections only apply transformations that are ecma 5 safe
 					// https://github.com/facebook/create-react-app/pull/4234
@@ -212,15 +212,16 @@ exports = module.exports = {
 					comparisons: false,
 					// Disabled because of an issue with Terser breaking valid code:
 					// https://github.com/facebook/create-react-app/issues/5250
-					// Pending futher investigation:
+					// Pending further investigation:
 					// https://github.com/terser-js/terser/issues/120
 					inline: 2,
-
-					passes: 2
 				},
 				mangle: {
 					safari10: true
 				},
+				// Added for profiling in devtools
+				keep_classnames: true,
+				keep_fnames: true,
 				output: {
 					ecma: 5,
 					comments: false,
@@ -231,6 +232,7 @@ exports = module.exports = {
 			},
 			parallel: true,
 			cache: true,
+			sourceMap: true,
 		})].filter(Boolean),
 		sideEffects: true,
 		splitChunks: {
