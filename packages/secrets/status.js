d611bb35ce2e6826e6202b83402c06a88261fa28
Jonathan Grimes <jsg2021@gmail.com>
2020-07-06 16:15:31 -0500

    No longer needed

--- packages/secrets/status.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-const trunk = (x, y) => x.length >= y ? x.substr(0, y - 4) + '...' : x;
-module.exports = x => {
-	process.stdout.clearLine();
-	process.stdout.cursorTo(0);
-	process.stdout.write(trunk(x || '', process.stdout.columns));
-};
