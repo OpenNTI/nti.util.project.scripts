413624f956859ef06c8e26acb3e7d43b72094ec9
Jonathan Grimes <jsg2021@gmail.com>
2020-07-17 11:35:05 -0500

    auth ux

--- packages/release/lib/config.js
+++ packages/release/lib/config.js
@@ -1,10 +1,13 @@
+import chalk from 'chalk';
 import netrc from 'netrc';
 import inquirer from 'inquirer';
 import octokit from '@octokit/rest';
 
+import {write} from './utils.js';
+
 let github;
 
-export async function getToken () {
+async function getToken () {
 	const rc = netrc();
 	try {
 		const token = rc['github.com']?.password;
@@ -14,29 +17,57 @@ export async function getToken () {
 
 		return token;
 	} catch {
-		const {save, ...auth} = await inquirer.prompt([
-			{
-				type: 'input',
-				message: 'Please enter your github username:',
-				name: 'name'
-			},
-			{
-				type: 'password',
-				message: 'Please enter your github personal access token:',
-				name: 'password'
-			},
-			{
-				message: 'Save token to config?',
-				name: 'save',
-				type: 'confirm'
-			}
-		]);
+		return promptForToken(rc);
+	}
+}
 
+
+async function promptForToken (rc, attempts = 0) {
+
+	if (attempts >= 3) {
+		// ✗, ✗, ✖, ✕ ?
+		write(chalk.reset.bold.red('✕'), chalk.bold.gray('Too many attempts'));
+		process.exit(1);
+	}
+
+	const {name, token} = await inquirer.prompt([
+		{
+			type: 'input',
+			message: 'Please enter your github username:',
+			name: 'name'
+		},
+		{
+			type: 'password',
+			message: 'Please enter your github personal access token:',
+			name: 'token'
+		}
+	]);
+
+	if (await checkCredentials(token)) {
+		const {save} = await inquirer.prompt([{
+			default:false,
+			message: 'Save token to config?',
+			name: 'save',
+			type: 'confirm'
+		}]);
 		if (save) {
-			netrc.save({...rc, 'github.com': auth});
+			netrc.save({...rc, 'github.com': {name, password: token}});
 		}
 
-		return auth.password;
+		return token;
+	}
+
+	return promptForToken(rc, attempts + 1);
+}
+
+async function checkCredentials (token) {
+	try {
+		const api = new octokit.Octokit({auth: `token ${token}`});
+		await api.users.getAuthenticated();
+		return true;
+	} catch (e) {
+		write(chalk.reset.bold.red('!'), chalk.bold.gray(e.message));
+		return false;
 	}
 }
 
