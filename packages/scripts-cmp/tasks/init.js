1d936b366e80d4899a1cc5b7bfa7206de631b3d6
Jonathan Grimes <jsg2021@gmail.com>
2020-08-19 13:41:21 -0500

    add storybook commands?

--- packages/scripts-cmp/tasks/init.js
+++ packages/scripts-cmp/tasks/init.js
@@ -3,7 +3,11 @@
 global.NTI_INIT_TO_REMOVE = [
 	'-test', //leave the test folder alone
 ];
-global.NTI_INIT_SCRIPTS = {start: 'cmp-scripts start'};
+global.NTI_INIT_SCRIPTS = {
+	start: 'cmp-scripts start',
+	storybook: 'start-storybook -p 6006',
+	'build-storybook': 'build-storybook'
+};
 
 global.NTI_INIT_DROP_DEPENDENCIES = require('@nti/app-scripts/package.json').dependencies;
 
