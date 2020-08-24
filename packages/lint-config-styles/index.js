cf045033e1e4235462fb823b2efe51512209c9b0
Jonathan Grimes <jsg2021@gmail.com>
2020-08-24 14:38:19 -0500

    Turn off stylelint:no-descending-specificity

--- packages/lint-config-styles/index.js
+++ packages/lint-config-styles/index.js
@@ -3,6 +3,7 @@
 module.exports = {
 	'extends': 'stylelint-config-standard',
 	'rules': {
+		'no-descending-specificity': false,
 		'indentation': 'tab',
 		'selector-list-comma-newline-after': null,
 		'selector-pseudo-class-no-unknown': [true, {
