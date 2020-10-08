734f325c416ce806f6689ceaff24ffc168329d4b
Jonathan Grimes <jsg2021@gmail.com>
2020-10-08 11:58:01 -0500

    make pre commit npx callable

---
+++ packages/command-pre-commit/index.js
@@ -0,0 +1,32 @@
+'use strict';
+const { execSync } = require('child_process');
+const { ESLint } = require('eslint');
+
+const run = x => execSync(x, {stdio: 'pipe'}).toString('utf8').trim();
+const isJS = RegExp.prototype.test.bind(/\.(t|m?j)sx?$/i);
+
+async function main () {
+	const eslint = new ESLint({ fix: true });
+	const files = (run('git diff --diff-filter=d --cached --name-only')?.split('\n') ?? [])
+		.filter(isJS)
+		.map(x => ({file: x, content: run(`git show ":${x}"`)}));
+
+	let errors = 0;
+	for (const {file, content} of files) {
+		const results = await eslint.lintText(content, {filePath: file});
+
+		errors = results.reduce((a,r) => a + r.errorCount, errors);
+
+		const formatter = await eslint.loadFormatter('stylish');
+		const resultText = formatter.format(results);
+		process.stderr.write(resultText);
+	}
+
+	process.exitCode = errors > 0 ? 1 : 0;
+}
+
+
+main().catch((error) => {
+	process.exitCode = 1;
+	console.error(error);
+});
--- packages/scripts-lib/bin/pre-commit-hook.sh
+++ packages/scripts-lib/bin/pre-commit-hook.sh
@@ -3,34 +3,4 @@
 # unset ATOM_HOME
 set -e
 export NODE_ENV=production
-node <<'JS_CODE'
-const { execSync } = require('child_process');
-const { ESLint } = require("eslint");
-
-const run = x => execSync(x, {stdio: 'pipe'}).toString('utf8').trim();
-const isJS = RegExp.prototype.test.bind(/\.(t|m?j)sx?$/i);
-
-(async function main() {
-	const eslint = new ESLint({ fix: true });
-	const files = (run('git diff --diff-filter=d --cached --name-only')?.split('\n') ?? [])
-		.filter(isJS)
-		.map(x => ({file: x, content: run(`git show ":${x}"`)}));
-
-	let errors = 0;
-	for (const {file, content} of files) {
-		const results = await eslint.lintText(content, {filePath: file});
-
-		errors = results.reduce((a,r) => a + r.errorCount, errors);
-
-		const formatter = await eslint.loadFormatter("stylish");
-		const resultText = formatter.format(results);
-		process.stderr.write(resultText);
-	}
-
-	process.exitCode = errors > 0 ? 1 : 0;
-
-})().catch((error) => {
-	process.exitCode = 1;
-	console.error(error);
-});
-JS_CODE
+npx @nti/pre-commit
