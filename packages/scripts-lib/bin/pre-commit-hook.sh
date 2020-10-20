4a7b6c5c93f367001ec3dda37ed58e753a08df55
Jonathan Grimes <jsg2021@gmail.com>
2020-10-20 13:24:47 -0500

    fork precommit on npm version

--- packages/scripts-lib/bin/pre-commit-hook.sh
+++ packages/scripts-lib/bin/pre-commit-hook.sh
@@ -5,5 +5,9 @@ set -e
 export NODE_ENV=production
 JS=$(git diff --diff-filter=d --cached --name-only ***.{cjs,js,jsx,mjs})
 if [[ ! -z "$JS" ]]; then
-	npx --yes @nti/pre-commit
+	if [[ `npx -v` = 7* ]]; then
+		npx --yes @nti/pre-commit
+	else
+		npx @nti/pre-commit
+	fi
 fi
