ecc6b425d3c7fc82b5aceb89d06bd741293fd65e
Jonathan Grimes <jsg2021@gmail.com>
2021-01-23 19:51:49 -0600

    prevent install, depend on being installed

--- packages/scripts-lib/bin/pre-commit-hook.sh
+++ packages/scripts-lib/bin/pre-commit-hook.sh
@@ -5,9 +5,5 @@ set -e
 export NODE_ENV=production
 CODE=$(git diff --diff-filter=d --cached --name-only ***.{cjs,js,jsx,mjs,css,scss,sass})
 if [[ ! -z "$CODE" ]]; then
-	if [[ `npx -v` = 7* ]]; then
-		npx --yes @nti/pre-commit
-	else
-		npx @nti/pre-commit
-	fi
+	npx --no-install @nti/pre-commit
 fi
