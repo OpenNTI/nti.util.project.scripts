4b79976d13a375ea7b2510c30fb2b4fb9fccdfbb
Jonathan Grimes <jsg2021@gmail.com>
2021-01-31 22:01:58 -0600

    Make Tests fail on Un-expected console.warn/error. Unhandled Promise Rejection taints exit code. (no silent passing)

--- packages/scripts-lib/config/jest/setup-after-env.js
+++ packages/scripts-lib/config/jest/setup-after-env.js
@@ -1,2 +1,16 @@
 'use strict';
+const { format } = require('util');
+
 global.beforeEach(() => {});
+
+global.console.error = (...args) => {
+	throw new Error(format(...args));
+};
+global.console.warn = (...args) => {
+	throw new Error(format(...args));
+};
+
+process.on('unhandledRejection', error => {
+	process.exitCode = process.exitCode || 1;
+	throw error;
+});
