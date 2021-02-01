236cbd9bbc425f110bb53d503773a2a224f3311f
Jonathan Grimes <jsg2021@gmail.com>
2021-01-31 21:34:54 -0600

    we do not have moment-timezone anymore

--- packages/scripts-lib/config/jest/setup-after-env.js
+++ packages/scripts-lib/config/jest/setup-after-env.js
@@ -1,9 +1,4 @@
 'use strict';
 global.beforeEach(() => {
-	try {
-		// patch moment to always return UTC, if its not installed, ignore.
-		require('moment-timezone').tz.guess = () => 'UTC';
-	} catch {
-		//
-	}
+
 });
