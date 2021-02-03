ea2f895a2438f5b715129e8f60f9d1c0603010ac
Jonathan Grimes <jsg2021@gmail.com>
2021-02-03 10:25:08 -0600

    move the npm workarounds into a independent module

---
+++ packages/command-workspace-post-install/index.js
@@ -0,0 +1,71 @@
+import {exec, execSync} from 'child_process';
+import {promises as fs} from 'fs';
+import {dirname, resolve} from 'path';
+import glob from 'glob';
+import ora from 'ora';
+
+async function run (cwd, command) {
+	return new Promise((fulfill, reject) => {
+		exec(command, {cwd}, (err, stdout, stderr) => {
+			if (err) {
+				console.error(stderr.toString('utf8'));
+				return reject(err);
+			}
+
+			fulfill(stdout.toString('utf8').trim());
+		});
+	});
+}
+
+async function install (dir) {
+	await run(dir, 'npm install --silent')
+		.catch(e => {
+			process.exitCode = 1;
+			console.error(e);
+		});
+}
+
+// npm7 currently has a bug where it will install the GIT version of deps even when its linked in the workspace...
+function cleanDupes () {
+	const candidates = glob.sync('./!(node_modules)/**/node_modules/@nti/');
+	const duplicates = candidates.filter((x, i, a) => !a.slice(0,i).find(y => x.startsWith(y)));
+	for(const x of duplicates) {
+		// remove the duplicate
+		fs.rm(x, {force: true, recursive:true})
+		// Remove parent directory (node_modules) if its empty
+			.then(() => fs.rmdir(dirname(x)))
+			// ignore errors
+			.catch(Boolean);
+	}
+}
+
+
+(async function main () {
+	const spinner = ora('Processing...').start();
+	cleanDupes();
+
+	await Promise.all([
+	// Lerna isn't compatible with this workspace layout, so we just install it by itself",
+		install(resolve('./scripts')),
+		//  There appears to be a bug with workspace installs not installing bundledDependencies... so we will install the service independently
+		install(resolve('./app/service')),
+	]);
+
+	spinner.stop();
+
+	if (process.exitCode) {
+		return;
+	}
+
+	execSync('npm run build', {
+		cwd: resolve('.'),
+		stdio: 'inherit'
+	});
+
+	// Server requires its dependencies (content packages/client settings) be locally present (under its own node_modules)
+	execSync('npm install', {
+		cwd: resolve('./server'),
+		stdio: 'inherit'
+	});
+
+})();
