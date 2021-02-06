344cf4893468a5119e3786c38e1e06658e5e9061
Jonathan Grimes <jsg2021@gmail.com>
2021-02-06 14:23:26 -0600

    add a workspace-refresh command

---
+++ packages/command-workspace-refresh/index.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import {execSync} from 'child_process';
+import {promises as fs} from 'fs';
+import {resolve} from 'path';
+import {promisify} from 'util';
+import glob from 'glob';
+import ora from 'ora';
+
+const find = promisify(glob);
+
+async function clean () {
+	const candidates = await find('**/node_modules');
+	const moduleDirs = candidates.filter((x, i, a) => !a.slice(0,i).find(y => x.startsWith(y)));
+	return Promise.all(moduleDirs.map(x =>
+		// remove the node_modules
+		fs.rm(x, {force: true, recursive:true})
+			// ignore errors
+			.catch(e => console.warn(e.message))));
+}
+
+
+(async function main () {
+	const spinner = ora('Cleaning...').start();
+	await clean();
+	spinner.stop();
+
+	execSync('npm install', {
+		cwd: resolve('.'),
+		stdio: 'inherit'
+	});
+})();
