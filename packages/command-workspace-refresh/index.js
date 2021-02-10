239e26f40cfaf94b01fa68e1d7fc677aa5dcf053
Jonathan Grimes <jsg2021@gmail.com>
2021-02-10 11:36:46 -0600

    pull projects while refreshing

--- packages/command-workspace-refresh/index.js
+++ packages/command-workspace-refresh/index.js
@@ -1,34 +1,76 @@
 #!/usr/bin/env node
-import {execSync} from 'child_process';
+import * as childProcess from 'child_process';
 import {promises as fs} from 'fs';
-import {resolve} from 'path';
+import {dirname, join, resolve} from 'path';
 import {promisify} from 'util';
 import glob from 'glob';
 import ora from 'ora';
 
 const find = promisify(glob);
+// const skipClean = !~process.argv.findIndex(x => /skip-clean/i);
 
 async function clean () {
+	const tmpDir = join(process.cwd(), '.trash');
+
 	const candidates = await find('**/node_modules');
 	const moduleDirs = candidates.filter((x, i, a) => !a.slice(0,i).find(y => x.startsWith(y)));
-	return Promise.all(moduleDirs.map(x =>
-		// remove the node_modules
-		fs.rm(x, {force: true, recursive:true})))
+	await Promise.all(moduleDirs.map(async x => {
+		await fs.mkdir(tmpDir, {recursive: true});
+		const trash = await fs.mkdtemp(join(tmpDir, 'trash-'));
+		await fs.rename(resolve(x), join(trash, 'node_modules'));
+	}));
+
+	// Don't wait for this, let it run in the background
+	fs.rm(tmpDir, {force: true, recursive:true})
+		.catch(er =>
+			console.warn('[warn] Could not remove ${trash}\n\tbecause: ', er.message));
 }
 
+async function update () {
+	const candidates = await find('**/.git');
+	const repos = candidates
+		.filter((x, i, a) =>
+			!x.includes('node_modules')
+			&& !a.slice(0,i).find(y => x.startsWith(y)))
+		.map(x => resolve(dirname(x)));
+
+	await Promise.all(repos.map(repo =>
+		exec(repo, 'git pull --rebase --autostash')
+			.catch(er => console.warn('[warn] %s: %s', repo, er))
+	));
+}
 
 (async function main () {
-	const spinner = ora('Cleaning...').start();
+
+	const spinner = ora('Pulling & Cleaning...').start();
 	try {
-		await clean();
+		await Promise.all([
+			update(),
+			clean()
+		]);
 		spinner.stop();
 	} catch (e) {
 		console.error('Could not fully clean node_modules: ', e.message);
 		process.exit(1);
 	}
 
-	execSync('npm install', {
+	childProcess.execSync('npm install', {
 		cwd: resolve('.'),
 		stdio: 'inherit'
 	});
 })();
+
+
+
+export async function exec (cwd, command) {
+	return new Promise((fulfill, reject) => {
+		childProcess.exec(command, {cwd}, (err, stdout, stderr) => {
+			if (err) {
+				console.error(stderr.toString('utf8'));
+				return reject(err);
+			}
+
+			fulfill(stdout.toString('utf8').trim());
+		});
+	});
+}
