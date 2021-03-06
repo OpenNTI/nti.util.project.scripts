28678f5eada76f414a38ebd977fbee2350bf8d3b
Jonathan Grimes <jsg2021@gmail.com>
2021-03-06 16:10:01 -0600

    add string extraction on builds

--- packages/scripts-app/tasks/utils/build-webpack.js
+++ packages/scripts-app/tasks/utils/build-webpack.js
@@ -1,51 +1,46 @@
 'use strict';
+const fs = require('fs');
+const { join } = require('path');
 const chalk = require('chalk');
 const { isCI } = require('ci-info');
 const formatMessages = require('webpack-format-messages');
+const { exec } = require('@nti/lib-scripts/tasks/utils/call-cmd');
 const webpack = require('webpack');
 const paths = require('../../config/paths');
 const pkg = require(paths.packageJson);
 
-module.exports = function build(
-	config = require('../../config/webpack.config')
-) {
+module.exports = main;
+
+async function build(config = require('../../config/webpack.config')) {
 	console.log('Creating a production build...');
 
 	const compiler = webpack(config);
 
-	return new Promise((resolve, reject) => {
-		compiler.run((err, stats) => {
-			if (err) {
-				return reject(err);
-			}
+	const stats = await new Promise((resolve, reject) =>
+		compiler.run((err, x) => (err ? reject(err) : resolve(x)))
+	);
 
-			const messages = formatMessages(stats);
+	const messages = formatMessages(stats);
 
-			if (messages.errors.length) {
-				return reject(new Error(messages.errors.join('\n\n')));
-			}
+	if (messages.errors.length) {
+		throw new Error(messages.errors.join('\n\n'));
+	}
 
-			if (
-				isCI &&
-				messages.warnings.length &&
-				!(pkg.build || {}).ignoreWarnings
-			) {
-				console.log(
-					chalk.yellow(
-						'\nTreating warnings as errors because isCI = true.\n' +
-							'Most CI servers set it automatically.\n'
-					)
-				);
-				return reject(new Error(messages.warnings.join('\n\n')));
-			}
+	if (isCI && messages.warnings.length && !(pkg.build || {}).ignoreWarnings) {
+		console.log(
+			chalk.yellow(
+				'\nTreating warnings as errors because isCI = true.\n' +
+					'Most CI servers set it automatically.\n'
+			)
+		);
+		throw new Error(messages.warnings.join('\n\n'));
+	}
 
-			return resolve({
-				stats,
-				warnings: messages.warnings,
-			});
-		});
-	}).then(handleResults, handleFailure);
-};
+	return {
+		stats,
+		warnings: messages.warnings,
+	};
+}
 
 function handleFailure(err) {
 	process.stdout.write('\r\x1b[K'); //clear the current line
@@ -70,5 +65,51 @@ function handleResults({ warnings }) {
 		);
 	} else {
 		console.log(chalk.green('Compiled successfully.\n'));
+	}
+}
+
+function computeChunkLog(stats) {
+	const data = stats
+		.toJson({}, true)
+		.children[0].chunks.sort((a, b) =>
+			a.entry !== b.entry
+				? a.entry
+					? 1
+					: -1
+				: // put initials
+				a.initial === b.initial
+				? 0
+				: a.initial
+				? -1
+				: 1
+		)
+		.map(x => x.files.filter(RegExp.prototype.test.bind(/\.js$/)))
+		.reduce((a, x) => [...a, ...x], []);
+
+	fs.writeFileSync(
+		join(paths.DIST_CLIENT, 'chunks.json'),
+		JSON.stringify(data, null, 2)
+	);
+}
+
+async function extractStrings() {
+	return exec(
+		paths.DIST_CLIENT,
+		[
+			process.argv[0],
+			join(__dirname, 'extract-strings.js'),
+			`"${paths.DIST_CLIENT}"`,
+		].join(' ')
+	);
+}
+
+async function main(config) {
+	try {
+		const results = await build(config);
+		computeChunkLog(results.stats);
+		await extractStrings();
+		handleResults(results);
+	} catch (er) {
+		handleFailure(er);
 	}
 }
---
+++ packages/scripts-app/tasks/utils/extract-strings.js
@@ -0,0 +1,47 @@
+/* eslint-disable no-undef */
+'use strict';
+const crypto = require('crypto');
+const { promises: fs } = require('fs');
+
+const mock = require('@nti/lib-scripts/tasks/utils/mock');
+
+(async () => {
+	const chunks = JSON.parse(
+		await fs.readFile('./chunks.json', { encoding: 'utf-8' })
+	);
+
+	const self = (globalThis.self = globalThis);
+	self.Ext = mock();
+	self.HTMLElement = self.HTMLElement || mock();
+	self.Image = mock();
+	self.crypto = Object.assign(crypto, { getRandomValues: x => x });
+	self.fetch = async () => Promise.resolve(mock());
+	self.history = mock();
+	self.location = mock();
+	self.screen = mock();
+	self.requestAnimationFrame = setImmediate;
+	self.cancelAnimationFrame = clearImmediate;
+	self.btoa = x => Buffer.from('' + x, 'binary').toString('base64');
+	self.atob = x => Buffer.from(x, 'base64').toString('ascii');
+
+	const pwd = process.cwd();
+	console.log('Extracting Strings...');
+
+	for (const chunk of chunks.reverse()) {
+		try {
+			require(`${pwd}/${chunk}`);
+		} catch (e) {
+			console.error(e.stack);
+			process.exitCode = 1;
+		}
+	}
+
+	await fs.writeFile(
+		'./strings.known.json',
+		JSON.stringify(__getLocalData().translations, null, 2)
+	);
+
+	// Cut off pending promises and tasks that might
+	// have been started by including app code.
+	process.exit(process.exitCode);
+})();
