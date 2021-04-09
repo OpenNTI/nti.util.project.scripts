8cb0f94ec8c4fe9ec67c8a86f5de596f79e95dba
Jonathan Grimes <jsg2021@gmail.com>
2021-04-09 18:26:41 -0500

    fix storybook

--- packages/scripts-cmp/config/storybook/globals.js
+++ packages/scripts-cmp/config/storybook/globals.js
@@ -1,3 +1,13 @@
+/*globals window, globalThis*/
 'use strict';
 require('regenerator-runtime');
+let g =
+	typeof globalThis !== 'undefined'
+		? globalThis
+		: typeof window === 'undefined'
+		? global
+		: window.global || window;
+if (!globalThis.global) {
+	globalThis.global = g;
+}
 global.$AppConfig = global.$AppConfig || { server: 'dataserver2' };
