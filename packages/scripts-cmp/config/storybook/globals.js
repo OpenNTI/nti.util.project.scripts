9718a9e927c82cd5107181af9904b42be39730f9
Jonathan Grimes <jsg2021@gmail.com>
2021-08-30 14:13:07 -0500

    fix storybook

--- packages/scripts-cmp/config/storybook/globals.js
+++ packages/scripts-cmp/config/storybook/globals.js
@@ -1,6 +1,7 @@
 /*globals DEV_DATA_SERVER_AUTH*/
 'use strict';
-require('@nti/lib-scripts/config/polyfills');
+require('regenerator-runtime');
+require('@nti/app-scripts/config/polyfills');
 
 global.$AppConfig =
 	typeof DEV_DATA_SERVER_AUTH !== 'undefined'
