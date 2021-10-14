8e33fd6eab83cf88173a5dbf6812f2b677d987e3
Jonathan Grimes <jsg2021@gmail.com>
2021-10-14 10:39:04 -0500

    We need to worktowards not using the localName in QA, but this exposed many tests relying on the localName. Putting it back for now.

--- packages/scripts-app/config/css-loaders.js
+++ packages/scripts-app/config/css-loaders.js
@@ -102,7 +102,7 @@ const loaders = (paths, options = {}) => {
 						exportGlobals: true,
 						exportLocalsConvention: 'camelCase',
 						localIdentName: PROD
-							? '[hash:base64]' //'[hash:base64]'
+							? '[local]--[hash:base64]' //'[hash:base64]'
 							: '[path][name]__[local]--[hash:base64]',
 					},
 				}),
