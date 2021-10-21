acfdba94a3910c5779d1e4427ed4046a133a153f
Jonathan Grimes <jsg2021@gmail.com>
2021-10-21 13:49:18 -0500

    support node 17

--- packages/scripts-lib/bin/run-task.js
+++ packages/scripts-lib/bin/run-task.js
@@ -1,6 +1,7 @@
 'use strict';
 const { totalmem } = require('os');
 const { spawnSync } = require('child_process');
+const semver = require('semver');
 const { resolveAppDir } = require('./resolve-app-dir');
 
 const inspect = process.argv.slice(3).some(x => x.startsWith('--inspect')); // --inspect-brk
@@ -23,6 +24,17 @@ module.exports = function run(scriptFile, name, args) {
 		{
 			cwd: resolveAppDir(process.cwd(), name),
 			stdio: 'inherit',
+			env: {
+				...process.env,
+				...(semver.lt(process.version, '17.0.0')
+					? null
+					: {
+							// node 17 changes to OpenSSL 3, which is stricter,
+							// and webpack and its various addons do not fully handle that yet,
+							// so we are going to add this flag to make node's relax OpenSSL relax for now
+							NODE_OPTIONS: ' --openssl-legacy-provider',
+					  }),
+			},
 		}
 	);
 
