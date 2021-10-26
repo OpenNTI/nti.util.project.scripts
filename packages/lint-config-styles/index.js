1c1842cf0634ac818b39ce10b00caa0788e74887
Jonathan Grimes <jsg2021@gmail.com>
2021-10-26 11:02:05 -0500

    migrate stylelint config

--- packages/lint-config-styles/index.js
+++ packages/lint-config-styles/index.js
@@ -1,30 +1,47 @@
 'use strict';
 
 module.exports = {
-	extends: [
-		'stylelint-config-standard',
-		'stylelint-config-css-modules',
-		'stylelint-config-prettier',
+	overrides: [
+		{
+			files: ['**/*.scss'],
+			extends: [
+				'stylelint-config-standard',
+				'stylelint-config-standard-scss',
+				'stylelint-config-prettier',
+			],
+		},
+
+		{
+			files: ['**/*.html'],
+			customSyntax: 'postcss-html',
+			extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+		},
+		{
+			files: ['**/*.css'],
+			extends: [
+				'stylelint-config-standard',
+				'stylelint-config-css-modules',
+				'stylelint-config-prettier',
+			],
+		},
+		{
+			files: [
+				'**/*.js',
+				'**/*.jsx',
+				'**/*.ts',
+				'**/*.tsx',
+				'**/*.mjs',
+				'**/*.cjs',
+			],
+			customSyntax: '@stylelint/postcss-css-in-js',
+			extends: [
+				'stylelint-config-standard',
+				'stylelint-config-css-modules',
+				'stylelint-config-prettier',
+			],
+		},
 	],
 	rules: {
-		'at-rule-no-unknown': [
-			true,
-			{
-				ignoreAtRules: [
-					'at-root',
-					'each',
-					'else',
-					'extend',
-					'for',
-					'function',
-					'if',
-					'include',
-					'mixin',
-					'value',
-					'use',
-				],
-			},
-		],
 		'no-invalid-position-at-import-rule': null,
 		'custom-property-empty-line-before': null,
 		'declaration-block-single-line-max-declarations': 3,
